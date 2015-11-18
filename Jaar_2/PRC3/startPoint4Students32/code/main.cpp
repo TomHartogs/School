b91b0c6a312b95ce61d93e6b4370a51529354944
TomHartogs <tomhartogs@gmail.com>
2015-11-18 21:13:10 +0100

    PRC Sluice and Sort added

---
+++ Jaar_2/PRC3/startPoint4Students32/code/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "FileStructure.h"
+#include "Key.h"
+
+int main()
+{
+    FileStructure f;
+    Key head;
+    
+    f.loadFile("data/gibberish.bin", head);
+
+    // next line is only to show what kind of data we're working with
+    // remove this line to increase performance!
+    head.print();
+    
+    // sort all data
+    // todo: your code here!
+    
+    // save sorted data into a new file called sorted.bin
+    f.saveFile(head, "sorted.bin");
+    
+    return 0;
+}
