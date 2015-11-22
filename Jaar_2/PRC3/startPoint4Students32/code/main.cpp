8caa3a574334b9f0a8f80a68e53b9d0448bcebbd
TomHartogs <tomhartogs@gmail.com>
2015-11-22 21:52:10 +0100

    clear make error PRC

--- Jaar_2/PRC3/startPoint4Students32/code/main.cpp
+++ Jaar_2/PRC3/startPoint4Students32/code/main.cpp
@@ -17,9 +17,9 @@ int main()
     // sort all data
     // todo: your code here!
     Key current;
-    current.setNext(head.getNext);
-    if(current.getText() > current.getNext()->getText())
-		swap(current, current.getNext());
+    //current.setNext(head.getNext);
+    //if(current.getText() > current.getNext()->getText())
+	//	swap(current, current.getNext());
     
     // save sorted data into a new file called sorted.bin
     f.saveFile(head, "sorted.bin");
